0ec1595c1b3acd382e49fac17ef02e08fdb3d242
mldn <395812217@qq.com>
2018-05-25 14:04:35 +0800

    change Hello.java File

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
 	public static void main(String args[]) {
-		System.out.println("Hello World !") ;
+		System.out.println("³éÑÌ !") ;
+		System.out.println("ºÈ¾Æ !") ;
+		System.out.println("ÌÌÍ· !") ;
+
 	}
 }
